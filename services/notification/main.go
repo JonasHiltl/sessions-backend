5422193f0bfb2c049231364c805cc8f649efe7aa
JonasHiltl <jonashiltl2003@gmail.com>
2022-03-27 22:40:02 +0200

    Stop throwing error if no .env file is found

--- services/notification/main.go
+++ services/notification/main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found")
 	}
 
 	opts := []gonats.Option{gonats.Name("Notification Service")}
