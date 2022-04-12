2d87288f993c8e5a240a0460e63fa8922204f92e
JonasHiltl <jonashiltl2003@gmail.com>
2022-04-12 17:10:03 +0200

    return user profile on partyByUser request

--- services/data_aggregator/internal/handler/party_handler/get_party_by_user.go
+++ services/data_aggregator/internal/handler/party_handler/get_party_by_user.go
@@ -3,19 +3,50 @@ package partyhandler
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jonashiltl/sessions-backend/packages/grpc/party"
+	pg "github.com/jonashiltl/sessions-backend/packages/grpc/profile"
 	"github.com/jonashiltl/sessions-backend/packages/utils"
+	"github.com/jonashiltl/sessions-backend/services/data_aggregator/internal/datastruct"
 )
 
 func (h *partyGatewayHandler) GetPartyByUser(c *fiber.Ctx) error {
 	uId := c.Params("id")
 	nextPage := c.Query("nextPage")
 
-	res, err := h.partyClient.GetByUser(c.Context(), &party.GetByUserRequest{UserId: uId, NextPage: nextPage})
+	partyRes, err := h.partyClient.GetByUser(c.Context(), &party.GetByUserRequest{UserId: uId, NextPage: nextPage})
 	if err != nil {
 		return utils.ToHTTPError(err)
 	}
 
-	// TODO: Decide if it's necessary to always get one/multiple stories of a party to have anything to show in the Frontend when viewing a Party
+	// get the profile of the party creator
+	profilesRes, err := h.profileClient.GetProfile(c.Context(), &pg.GetProfileRequest{Id: uId})
+	if err != nil {
+		return utils.ToHTTPError(err)
+	}
+
+	var aggParties []datastruct.AggregatedParty
+	for _, p := range partyRes.Parties {
+		aggParties = append(aggParties, datastruct.AggregatedParty{
+			Id:            p.Id,
+			Creator:       profilesRes,
+			Title:         p.Title,
+			IsPublic:      p.IsPublic,
+			Lat:           p.Lat,
+			Long:          p.Long,
+			StreetAddress: p.StreetAddress,
+			PostalCode:    p.PostalCode,
+			State:         p.State,
+			Country:       p.Country,
+			// TODO: we might want to fetch some stories of the party but would have to do this for all party returned of this user
+			// Stories:
+			StartDate: p.StartDate,
+			CreatedAt: p.CreatedAt,
+		})
+	}
+
+	res := datastruct.PagedAggregatedParty{
+		Parties:  aggParties,
+		NextPage: partyRes.NextPage,
+	}
 
 	return c.Status(fiber.StatusOK).JSON(res)
 }
