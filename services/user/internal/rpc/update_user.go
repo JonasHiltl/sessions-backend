0344060ddba1e8cc6a542092d18ff09651fbd3e5
JonasHiltl <jonashiltl2003@gmail.com>
2022-04-18 16:23:57 +0200

    Check Email on update

--- services/user/internal/rpc/update_user.go
+++ services/user/internal/rpc/update_user.go
@@ -3,12 +3,15 @@ package rpc
 import (
 	"context"
 	"errors"
+	"net/mail"
 	"strings"
 
 	ug "github.com/jonashiltl/sessions-backend/packages/grpc/user"
 	"github.com/jonashiltl/sessions-backend/packages/utils"
 	"github.com/jonashiltl/sessions-backend/services/user/internal/datastruct"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *userServer) UpdateUser(ctx context.Context, req *ug.UpdateUserRequest) (*ug.User, error) {
@@ -39,6 +42,14 @@ func (s *userServer) UpdateUser(ctx context.Context, req *ug.UpdateUserRequest) 
 			return nil, utils.HandleError(err)
 		}
 		du.PasswordHash = hash
+	}
+
+	if req.Email != "" {
+		_, err = mail.ParseAddress(req.Email)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, "Invalid Email")
+		}
+		du.Email = req.Email
 	}
 
 	u, err := s.us.Update(ctx, du)
